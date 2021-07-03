2ea2847a0e4bd5af5f18dc44b6bb5a86ca69319d
Arturo Marquez <arturomf94@gmail.com>
2021-07-03 18:01:19 -0500

    Add first test

--- tests/cosa/test_cosa.py
+++ tests/cosa/test_cosa.py
@@ -1,12 +1,17 @@
 """Test main `Cosa` class functionality.
 """
+import pytest
 from cosa import Cosa
 
 
 def test_read():
-    """ Test reading functionality.
-    """
+    """Test reading functionality."""
     cosa = Cosa()
     # Existing file
     cosa.read("tests/test_files/miso.jpg")
     assert cosa.image is not None
+    # Non-existing file
+
+    assert cosa.image is not None
+    with pytest.raises(ValueError):
+        cosa.read("tests/test_files/fake.jpg")
