30278a51929dac8687ef8d9f84f32b1d9c95794a
Arturo Marquez Flores <arturomf94@gmail.com>
2022-01-16 13:19:01 -0600

    Add test for elastic transform

--- tests/cosa/test_cosa.py
+++ tests/cosa/test_cosa.py
@@ -24,3 +24,14 @@ def test_k_rep():
     assert cosa.image is not None
     cosa.transform("k_rep")
     assert cosa.transformed is not None
+
+
+def test_elastic():
+    """Test `elastic` transformation."""
+    cosa = Cosa()
+    # Existing file
+    cosa.read("tests/test_files/miso.jpg")
+    # Non-existing file
+    assert cosa.image is not None
+    cosa.transform("elastic")
+    assert cosa.transformed is not None
