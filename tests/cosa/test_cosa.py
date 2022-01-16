cbe7f1690f8c55851161388e1e94165dc45c9d5a
Arturo Marquez Flores <arturomf94@gmail.com>
2022-01-16 13:47:58 -0600

    Add write test and improve other tests

--- tests/cosa/test_cosa.py
+++ tests/cosa/test_cosa.py
@@ -2,24 +2,43 @@
 """
 import pytest
 from cosa import Cosa
+from pathlib import Path
 
 
 def test_read():
     """Test reading functionality."""
+    input_path = "tests/test_files/miso.jpg"
+    fake_path = "tests/test_files/fake.jpg"
     cosa = Cosa()
     # Existing file
-    cosa.read("tests/test_files/miso.jpg")
+    cosa.read(input_path)
     # Non-existing file
     assert cosa.image is not None
     with pytest.raises(ValueError):
-        cosa.read("tests/test_files/fake.jpg")
+        cosa.read(fake_path)
+
+
+def test_write():
+    """Test saving functionality."""
+    input_path = "tests/test_files/miso.jpg"
+    output_path = "tests/test_files/miso_write.jpg"
+    cosa = Cosa()
+    # Existing file
+    cosa.read(input_path)
+    # Non-existing file
+    assert cosa.image is not None
+    cosa.transform("elastic")
+    assert cosa.transformed is not None
+    cosa.write(output_path)
+    assert Path(output_path).is_file()
 
 
 def test_k_rep():
     """Test `k_rep` transformation."""
+    input_path = "tests/test_files/miso.jpg"
     cosa = Cosa()
     # Existing file
-    cosa.read("tests/test_files/miso.jpg")
+    cosa.read(input_path)
     # Non-existing file
     assert cosa.image is not None
     cosa.transform("k_rep")
@@ -28,9 +47,10 @@ def test_k_rep():
 
 def test_elastic():
     """Test `elastic` transformation."""
+    input_path = "tests/test_files/miso.jpg"
     cosa = Cosa()
     # Existing file
-    cosa.read("tests/test_files/miso.jpg")
+    cosa.read(input_path)
     # Non-existing file
     assert cosa.image is not None
     cosa.transform("elastic")
