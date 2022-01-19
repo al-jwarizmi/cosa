399e77af479ea18c6f09913f06ff89c4dc59dae6
Arturo Marquez Flores <arturomf94@gmail.com>
2022-01-19 14:09:43 -0600

    Bump version (#7)

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-setup(
-    name="co_sa",
-    version="0.1",
-    description="Computer Optic Semantics",
-    packages=["cosa"],
-    author="Alfredo Lozano",
-    author_email="lozanoa94@gmail.com",
-    install_requires=["scipy", "scikit-image", "scikit-learn", "numpy"],
-    zip_safe=False,
+setup(name='co_sa',
+    version='0.0.2',
+    description='Computer Optic Semantics',
+    packages=['cosa'],
+    author = 'Alfredo Lozano',
+    author_email = 'lozanoa94@gmail.com',
+    install_requires=[
+   'scipy',
+   'scikit-image',
+   'scikit-learn',
+   'numpy'
+   ],
+   zip_safe=False,
 )
