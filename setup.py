03ad41cfcab6565c6fa19f6251def28ba5f42536
Arturo Marquez Flores <arturomf94@gmail.com>
2022-03-13 11:34:18 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cosabot",
-    version="0.0.6",
+    version="0.0.7",
     description="Computer Optic Semantics",
     packages=["cosa"],
     author="Alfredo Lozano",
